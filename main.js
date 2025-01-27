2241ff6660a6d3f70a3a0aea5b094797d0bea346
supitta <supitta.c@ku.th>
2025-01-27 12:31:33 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,208 @@
+// String,Number,Boolean,Object,Array
+
+// String
+ // let name ='John';
+//const idcard = '1234';
+
+//console.log(firstname);
+//Number
+//let age = 25;
+//let firstname = 5.9;
+
+//Boolean
+//let isMarried = false;
+//firstname = 'xxxx';
+//idcard = '5678';
+//console.log('my name is', firstname, 'and I am',age,'year old.')
+
+
+/*
+== เท่ากับ
+!= ไม่เท่ากับ
+> มากกว่า
+<น้อยกว่า
+>= มากกว่าหรือเท่ากับ
+<= น้อยกว่าหรือเท่ากับ
++บวก
+-ลบ
+*คูณ
+/หาร
+%หารเอาเศษ
+*/
+/*
+>= 80 เป็นเกรด A
+>= 70 เป็นเกรด B
+>= 60 เป็นเกรด C
+>= 50 เป็นเกรด D
+&& และ
+|| หรือ
+! not หรือ ไม่
+*/
+
+//let number1 = 5;
+//let number2 = 10;
+
+//let result = number1 + number2;
+//console.log('new number is ',result); // 48
+
+//let number1 = 5;
+//let number2 = 5;
+
+//let condition1 = number1 + number2;//Boolean ค่าที่ได้จะเป็น true หรือ false
+//console.log('result of condition is',condition1);
+
+
+//let number1 = 5;
+//let number2 = 10;
+
+//if - else condition
+
+//if (number1 >= number2){
+    //console.log('this is if');
+//} else if (number1 == number2) {
+    //console.log('this is else if');
+//} else {
+   // console.log('this is else');
+//}
+
+//let score = prompt('Enter your score: ');
+////console.log('your score is'+score);
+//if - else condition
+//if (score >= 80) {
+    //console.log('you are grade A');
+//}else if (score >= 70){
+    //console.log('you are grade B');
+//}else if (score >= 60){
+    //console.log('you are grade C');
+//}else if (score >= 50){
+    //console.log('you are grade D');
+//}else{
+    //console.log('you are grade F');
+//}
+
+//let number1 = 5;
+//let number2 = 10;
+    // true && false = false
+//let condition = number1 >= 3||number2 >= 10;
+//console.log('result of condition',condition);
+
+//let age = 30;
+//let gender = 'male';
+//true && true = true
+//if (age >= 20 && gender =='male'){
+//    console.log('You are male adult');
+//}
+
+//let number = 25;
+
+//let (!(number % 2 == 0)) {
+//    console.log('you are even number');
+//}
+    
+
+//let counter = 0;
+
+//while (counter <= 10){ //true
+//    console.log('whaile loop');
+//    counter = counter + 1;
+//}
+
+//array
+
+/*let age1 = 20;
+let age2 = 30;
+let age3 = 40;
+let age4 = 50;
+console.log(age1,age2,age3,age4);
+
+let age =[20,30,40,50];
+console.log('new age',ages[2]);
+console.log('age list',ages);
+
+//1.แทนที่ค่าใน array
+ages = [45,55];
+console.log('new age',ages);
+//เดี่ยวกลับมาเขียนต่อ
+
+let ages =[90,60,,40,45,50];
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//object + array
+
+let score = 50
+let grade = ''
+
+if (score >= 80){
+    grade = A
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+array
+*/
+/*
+let scores = [10,20,30,40,50,60];
+for (let index = 0; index < score.length; index++){
+    console.log(score[index]);
+
+}/*
+scores[0]=scores[0]*2;
+scores[0]=scores[0]*2;
+scores[0]=scores[0]*2;
+scores[0]=scores[0]*2;
+scores[0]=scores[0]*2;
+*/
+/*
+
+scores = scores.map((s) =>{
+    return s * 2 ;
+});
+
+
+score.forEach((s)=> {
+    console.log('new score:',s)
+})
+*/
+
+let scores = [10,20,30,40];
+let newScortes = []
+
+for (let index = 0 ; index < scores.length; index++){
+    console.log('')
+}
+
+
+
+
+
+
+
+/*
+ojects function
+*/
+
